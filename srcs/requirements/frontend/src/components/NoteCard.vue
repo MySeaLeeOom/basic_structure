0316f90a3aca39dac55524f9ca008a8a6d30e119
Sysex89 <40394815+Sysex89@users.noreply.github.com>
2026-01-30 19:52:44 +0100

    feat: add new Vue components for BaseButton, BaseLogo, NoteCard, NoteFolder, NoteListItem, SettingsButton, SideBar, VersionDropDownMenu, and WorkspaceTabs

---
+++ srcs/requirements/frontend/src/components/NoteCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="note-card">
+    <div class="header"></div>
+    <textarea></textarea>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
---
+++ srcs/requirements/frontend/src/components/NoteListItem.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="note-list-item">
+    <body>
+      noteItem
+    </body>
+  </div>
+</template>
+<script lang="ts" setup></script>
