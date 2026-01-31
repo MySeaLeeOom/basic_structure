f6965859206a2ea3971ccbd449fa172858f2f165
Caro Silva Rode <carolinasilvarode@gmail.com>
2026-01-31 13:28:56 +0100

    api gateway

--- srcs/requirements/gateway/src/main.py
+++ srcs/requirements/gateway/src/main.py
@@ -1,10 +1,13 @@
 from fastapi import Request, Depends, FastAPI, HTTPException, status, Response
-from starlette.responses import FileResponse 
+from starlette.responses import FileResponse
+import httpx
 
 app = FastAPI()
 
+http_client = httpx.AsyncClient()
+
 SERVICES = {
-	"notes": "http://notes:8000"
+	"/api/notes": "http://notes:8000/api/notes"
 }
 
 # index.html  had to be moved from nginx to here
@@ -19,5 +22,29 @@ async def forward(request: Request):
 	print(f"the service is: {service}")
 	if upstream is None:
 		return Response(status_code=404)
-	print(f"forward to: {service}, url = {request.url}")
-	return Response(status_code=200)
+	body = await request.body()
+	headers = dict(request.headers)
+	method = request.method
+	query_params = dict(request.query_params)
+	#
+	print(f"  body: {body}")
+	print(f"  headers: {headers}")
+	print(f"  method: {method}")
+	print(f"  query_params: {query_params}")
+	print(f"  upstream: {upstream}")
+	#
+	response = await http_client.request(
+		method,
+		str(upstream),
+		content=body,
+		params=query_params,
+		headers=headers
+	)
+	print(f"-> reponse: {response}")
+	#
+	#
+	return Response(
+        content=response.content,
+        status_code=response.status_code,
+        headers=dict(response.headers),
+    )
