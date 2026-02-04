fa093880b1bfbab269e1f0956dc1cc2436645d5b
Maarten Hoff <147248323+MaartenHoff@users.noreply.github.com>
2026-02-04 15:42:51 +0100

    added Delete end point + index.html updated to test the new endpoint

--- srcs/requirements/notes-rs/src/main.rs
+++ srcs/requirements/notes-rs/src/main.rs
@@ -2,7 +2,7 @@ mod models;
 mod handlers;
 
 use axum::{
-	routing::{get, post},
+	routing::{get},
 	Router,
 };
 use sqlx::PgPool;
@@ -14,7 +14,7 @@ async fn main() {
 
 	let app = Router::new()
 		.route("/api/notes", get(handlers::get_all_notes).post(handlers::post_note))
-		.route("/api/note/{id}", get(handlers::get_note))
+		.route("/api/notes/{id}", get(handlers::get_note).delete(handlers::del_note))
 		.with_state(db_pool);
 
 	let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
