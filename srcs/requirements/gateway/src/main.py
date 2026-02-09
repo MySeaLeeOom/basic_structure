be4a934283b661c3f3d3380372cfccee428cb8f4
cat <myakoven@student.42berlin.de>
2026-02-09 16:27:21 +0100

    ...

--- srcs/requirements/gateway/src/main.py
+++ srcs/requirements/gateway/src/main.py
@@ -157,7 +157,20 @@ async def forward(request: Request, path: str):
 	if upstream is None:
 		return Response(status_code=404)
 
+	# where is the user trying to go?
+	# service = request.url.path
+	# 
+	# # if it's an API endpoint, forward to the relevant service
+	# # otherwise, forward to the frontend service
+	# if service.startswith("/api"):
+	# 	upstream = SERVICES.get(service, None)
+	# else:
+	# 	upstream = SERVICES.get("/", None)
+	# print(f"the service is: {service}")
 
+	# # if it's not a thing, 404
+	# if upstream is None:
+	# 	return Response(status_code=404)
 
 	# craft request
 	body = await request.body()
@@ -189,5 +202,3 @@ async def forward(request: Request, path: str):
         status_code=response.status_code,
         headers=dict(response.headers),
     )
-
-
