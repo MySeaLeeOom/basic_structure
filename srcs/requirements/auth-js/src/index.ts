2dd057316a6bd8f86520358c9f2d9eb9daeacd54
cat <myakoven@student.42berlin.de>
2026-02-11 03:12:21 +0100

    prepare auth_db: fastify, ts,  postgres connection, env vars

---
+++ srcs/requirements/auth-js/src/index.ts
@@ -0,0 +1,43 @@
+import fastify from 'fastify'
+import postgres from '@fastify/postgres'
+import { readFileSync } from 'node:fs'; //for getting the secret from a file
+
+import { randomUUID } from 'node:crypto';
+
+// const sessionID = randomUUID(); //this is for the UUID/session
+// AUTH_DB_URL=postgres://${AUTH_DB_USER}:${AUTH_DB_PASSWORD}@${POSTGRES}/${AUTH_DB_NAME} 
+
+
+const server = fastify()
+
+const user = process.env.AUTH_DB_USER as string;
+const db_name = process.env.AUTH_DB_NAME as string;
+const host = process.env.POSTGRES_SERVICE as string;
+const auth_pass = readFileSync('/run/secrets/auth_db_password', 'utf8').trim();
+const connectionString = `postgres://${user}:${auth_pass}@${host}/${db_name}`;
+
+// this register db ass a plugin, the server makes sure the connection is there  
+// (pool of connections)
+server.register(postgres, {connectionString: connectionString});
+
+server.get('/ping', async (request, reply) => {
+  return 'pong\n'
+})
+
+// logic for initial login
+server.get('/api/auth/login', async (request, reply) => {
+})
+
+// separate one for checking cookie
+server.get('/api/auth/', async (request, reply) => {
+
+})
+
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
