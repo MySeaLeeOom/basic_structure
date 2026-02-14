36c01d788df0f86a76a45ff99a3931a6e7dabff3
Sysex89 <40394815+Sysex89@users.noreply.github.com>
2026-02-14 17:17:07 +0100

    update previous models in models.rs to new schema

--- srcs/requirements/notes-rs/src/models.rs
+++ srcs/requirements/notes-rs/src/models.rs
@@ -1,14 +1,21 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use chrono::{DateTime, Utc};
 
 #[derive(Serialize, Deserialize, sqlx::FromRow)]
 pub struct Note {
-	pub id: i32,
+	pub id: Uuid,
+	pub doc_state: Vec<u8>,
+	pub created_at: DateTime<Utc>,
+	pub updated_at: DateTime<Utc>,
 	pub title: String,
 	pub content: String,
 }
 
 #[derive(Deserialize)]
 pub struct CreateNote {
-	pub title: String,
-	pub content: String,
+	pub doc_state: Vec<u8>,
+	pub owner_id: Option<Uuid>,
+	pub title: Option<String>,
+	pub content: Option<String>,
 }
