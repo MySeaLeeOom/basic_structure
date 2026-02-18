06f6e96a96fea84a859f3ae41d6725007e4f4ba6
cat <myakoven@student.42berlin.de>
2026-02-18 23:44:04 +0100

    chore: keep other learning versions of auth, gitignore

--- srcs/requirements/auth/drizzle.config.ts
+++ srcs/requirements/auth/drizzle.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 import {databaseUrl} from "./src/db/connections"
 
+// Configure drizzle-kit migration tool
+
 export default defineConfig({
 	out: "./drizzle",
 	schema: "./src/db/schema.ts",
