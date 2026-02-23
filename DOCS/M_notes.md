04ddac76f6312ef02ac894ac6f0cbdc4d77ede39
cat <myakoven@student.42berlin.de>
2026-02-23 02:47:12 +0100

    feat(auth): add migration code to index.ts
    The command pnpm db:generate must run locally, if index.ts notices new migration file it will run it in docker

--- DOCS/M_notes.md
+++
@@ -1,4 +0,0 @@
-## Why passwords in secrets?
-
-Commands like ps aux or docker inspect can expose env variables. They are also broadcast to sidecar containers. Secrets remain inside. 
-
--- srcs/requirements/auth/src/db/schema.ts
+++ srcs/requirements/auth/src/db/schema.ts
@@ -11,7 +11,7 @@ export const users= pgTable('users', {
 	id: serial('id').primaryKey(),
 	provider: providerEnum('provider').notNull().default('local'),
 	provider_id: text('provider_id').notNull(),
-	email: text('emailAddress'),
+	email: text('email'),
 	passwordHash: text('password_hash'),
 	role: roleEnum('role').notNull(). default('user'),
 	status:userStatusEnum('status').notNull().default('active'),
