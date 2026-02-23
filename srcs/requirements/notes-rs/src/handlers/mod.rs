3147fba808b6cd54819ffc546bbeb2c78d31f5db
Maarten Hoff <147248323+MaartenHoff@users.noreply.github.com>
2026-02-23 17:06:20 +0100

    initial structure of the implentation of CRDTs in a hybrid structure

--- srcs/requirements/notes-rs/src/handlers/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod notes;
-pub mod persistence;
-pub mod websocket;
-
-pub use notes::{create_note, delete_note, get_all_notes, get_note};
-pub use websocket::ws_sync;
---
+++ srcs/requirements/notes/src/models.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, sqlx::FromRow)]
+pub struct Note {
+	pub id: i32,
+	pub title: String,
+	pub content: String,
+}
+
+#[derive(Deserialize)]
+pub struct CreateNote {
+	pub title: String,
+	pub content: String,
+}
