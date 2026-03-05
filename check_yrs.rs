8c9129cef045234377750cb3a4c6f934fa072bf0
Maarten Hoff <147248323+MaartenHoff@users.noreply.github.com>
2026-03-05 12:34:12 +0100

    removed my tester file: check_yrs.rs

--- check_yrs.rs
+++
@@ -1,7 +0,0 @@
-use yrs::{Doc, Transact, StateVector};
-fn main() {
-    let doc = Doc::new();
-    let txn = doc.transact();
-    let state = txn.encode_state_as_update_v1(&StateVector::default());
-    println!("{:?}", state);
-}
