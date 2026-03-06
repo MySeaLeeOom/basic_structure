ecfaa21b4c8a5e50aad07dfc2ed6fb7e8ec7d66c
cat <myakoven@student.42berlin.de>
2026-03-06 15:56:44 +0100

    db migration check

---
+++ srcs/requirements/auth/drizzle/0000_colorful_steel_serpent.sql
@@ -0,0 +1,30 @@
+CREATE TYPE "public"."provider_type" AS ENUM('github', 'local', 'google', '42');--> statement-breakpoint
+CREATE TYPE "public"."role" AS ENUM('user', 'admin');--> statement-breakpoint
+CREATE TYPE "public"."status" AS ENUM('active', 'blocked', 'suspended');--> statement-breakpoint
+CREATE TABLE "sessions" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"token" uuid DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" integer NOT NULL,
+	"user_role" "role" NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	"user_agent" text,
+	"ip_address" text,
+	CONSTRAINT "sessions_token_unique" UNIQUE("token")
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"login_name" text NOT NULL,
+	"provider" "provider_type" NOT NULL,
+	"provider_id" text NOT NULL,
+	"email" text,
+	"password_hash" text,
+	"role" "role" DEFAULT 'user' NOT NULL,
+	"status" "status" DEFAULT 'active' NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	CONSTRAINT "users_login_name_unique" UNIQUE("login_name"),
+	CONSTRAINT "users_email_unique" UNIQUE("email"),
+	CONSTRAINT "user_provider_id_unique" UNIQUE("provider","provider_id")
+);
+--> statement-breakpoint
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
