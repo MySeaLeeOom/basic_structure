e51e6a661e3d4df874d0891aef852256ca6ad0a7
Maarten Hoff <147248323+MaartenHoff@users.noreply.github.com>
2026-03-20 18:34:18 +0100

    removed: error_page 401 = @login

--- srcs/requirements/postgres/init/02-init.sql
+++ srcs/requirements/postgres/init/02-init.sql
@@ -1,15 +1,16 @@
 -- the container - managed by notes.
 CREATE TABLE IF NOT EXISTS notes (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    title VARCHAR(255) NOT NULL DEFAULT 'Untitled', -- for simplicity here for now :)
-    owner_id UUID, -- more logic when we combine with auth
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	title VARCHAR(255) NOT NULL DEFAULT 'Untitled', -- for simplicity here for now :)
+	owner_id UUID, -- more logic when we combine with auth
+	-- user_ids UUID[]
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 -- the content - managed by editor.
 CREATE TABLE IF NOT EXISTS note_states (
-    note_id UUID PRIMARY KEY REFERENCES notes(id) ON DELETE CASCADE,
-    state_vector BYTEA NOT NULL,
-    last_saved_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+	note_id UUID PRIMARY KEY REFERENCES notes(id) ON DELETE CASCADE,
+	state_vector BYTEA NOT NULL,
+	last_saved_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
