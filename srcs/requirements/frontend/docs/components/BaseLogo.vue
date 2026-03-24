fad454b74cf98645c3834210aed8705b6eef5863
aydiler <ahmet.diler@hotmail.de>
2026-03-24 05:58:46 +0100

    docs(frontend): remove component reference files from docs

--- srcs/requirements/frontend/docs/components/BaseLogo.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="base-logo">
-    <body>
-      myCelium
-    </body>
-  </div>
-</template>
-<script lang="ts" setup></script>
--- srcs/requirements/frontend/docs/components/NoteCard.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="note-card">
-    <div class="header"></div>
-    <textarea></textarea>
-  </div>
-</template>
-
-<script lang="ts" setup></script>
