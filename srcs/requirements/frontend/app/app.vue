a93b66982718779e3c29582a34d95a2dc2b66d56
cat <myakoven@student.42berlin.de>
2026-04-12 14:08:19 +0200

    fix weird flickers

--- srcs/requirements/frontend/app/app.vue
+++ srcs/requirements/frontend/app/app.vue
@@ -1,11 +1,9 @@
 <script lang="ts" setup>
-import { computed, onMounted } from 'vue';
+import { computed } from 'vue';
 import Header from "./components/Header.vue"
 import VoltConfirmDialog from "./volt/ConfirmDialog.vue"
-import { useAuthStore } from './stores/authStore';
 import { useUiI18n } from './composables/useUiI18n';
 
-const authStore = useAuthStore();
 const { locale } = useUiI18n();
 
 useHead({
@@ -13,10 +11,6 @@ useHead({
 		lang: computed(() => locale.value),
 		dir: computed(() => locale.value === 'ar' ? 'rtl' : 'ltr'),
 	},
-});
-
-onMounted(() => {
-	authStore.checkAuth();
 });
 </script>
 
